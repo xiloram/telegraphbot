1aa408e8bb4c11d2c2e3c6f2779f00abcd08d336
xiloram <109920108+xiloram@users.noreply.github.com>
2023-03-20 17:16:35 +0000

    bot

---
+++ bot.py
@@ -0,0 +1,37 @@
+import os
+from telegraph import upload_file
+import pyrogram
+from pyrogram import filters, Client
+from sample_config import Config
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery)
+
+
+Tgraph = Client(
+   "Telegra.ph Uploader",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+)
+
+@app.on_message(filters.command("telegraph"))
+@capture_err
+async def paste(_, message: Message):
+    reply = message.reply_to_message
+
+    if not reply or not reply.text:
+        return await message.reply("Reply to a text message")
+
+    if len(message.command) < 2:
+        return await message.reply("**Usage:**\n /telegraph [Page name]")
+
+    page_name = message.text.split(None, 1)[1]
+    page = telegraph.create_page(
+        page_name, html_content=(reply.text.html).replace("\n", "<br>")
+    )
+    return await message.reply(
+        f"**Posted:** {page['url']}",
+        disable_web_page_preview=True,
+    )
